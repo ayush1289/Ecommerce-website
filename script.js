2c80c303469a4647e13aa6b027491db4206e4222
ayush1289 <ayushshankarpurkar11@gmail.com>
2022-12-01 02:39:12 +0530

    added transactions page and added buttons access

---
+++ script.js
@@ -0,0 +1,65 @@
+// shoes section
+
+const b1 = document.getElementById('b1');
+
+b1.addEventListener("mouseover",() => {
+    b1.innerHTML='aukat ke bahar!';
+});
+b1.addEventListener("mouseout", () => {
+    b1.innerHTML='Buy Now';
+});
+b1.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
+
+
+const b2 = document.getElementById('b2');
+
+b2.addEventListener("mouseover",() => {
+    b2.innerHTML='aukat ke bahar!';
+});
+b2.addEventListener("mouseout", () => {
+    b2.innerHTML='Buy Now';
+});
+
+const b3 = document.getElementById('b3');
+
+b3.addEventListener("mouseover",() => {
+    b3.innerHTML='aukat ke bahar!';
+});
+b3.addEventListener("mouseout", () => {
+    b3.innerHTML='Buy Now';
+});
+
+const b4 = document.getElementById('b4');
+
+b4.addEventListener("mouseover",() => {
+    b4.innerHTML='aukat ke bahar!';
+});
+b4.addEventListener("mouseout", () => {
+    b4.innerHTML='Buy Now';
+});
+
+const b5 = document.getElementById('b5');
+
+b5.addEventListener("mouseover",() => {
+    b5.innerHTML='aukat ke bahar!';
+});
+b5.addEventListener("mouseout", () => {
+    b5.innerHTML='Buy Now';
+});
+
+const b6 = document.getElementById('b6');
+
+b6.addEventListener("mouseover",() => {
+    b6.innerHTML='aukat ke bahar!';
+});
+b6.addEventListener("mouseout", () => {
+    b6.innerHTML='Buy Now';
+});
+
+// clothing section
+
+
+

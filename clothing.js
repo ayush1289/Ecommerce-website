47240be4b988e16b839ce107fd39d4f5e737fb81
ayush1289 <ayushshankarpurkar11@gmail.com>
2022-12-05 17:27:58 +0530

    added buttons and a new js file for clothes

---
+++ clothing.js
@@ -0,0 +1,82 @@
+// clothing section
+
+
+const l1 = document.getElementById('l1');
+
+l1.addEventListener("mouseover",() => {
+    l1.innerHTML='$400';
+});
+l1.addEventListener("mouseout", () => {
+    l1.innerHTML='Buy Now';
+});
+l1.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
+
+
+const l2 = document.getElementById('l2');
+
+l2.addEventListener("mouseover",() => {
+    l2.innerHTML='$200';
+});
+l2.addEventListener("mouseout", () => {
+    l2.innerHTML='Buy Now';
+});
+l2.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
+
+const l3 = document.getElementById('l3');
+
+l3.addEventListener("mouseover",() => {
+    l3.innerHTML='$290';
+});
+l3.addEventListener("mouseout", () => {
+    l3.innerHTML='Buy Now';
+});
+l3.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
+
+const l4 = document.getElementById('l4');
+
+l4.addEventListener("mouseover",() => {
+    l4.innerHTML='$280';
+});
+l4.addEventListener("mouseout", () => {
+    l4.innerHTML='Buy Now';
+});
+l4.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
+
+const l5 = document.getElementById('l5');
+
+l5.addEventListener("mouseover",() => {
+    l5.innerHTML='$190';
+});
+l5.addEventListener("mouseout", () => {
+    l5.innerHTML='Buy Now';
+});
+l5.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
+
+const l6 = document.getElementById('l6');
+
+l6.addEventListener("mouseover",() => {
+    l6.innerHTML='$120';
+});
+l6.addEventListener("mouseout", () => {
+    l6.innerHTML='Buy Now';
+});
+l6.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
+
--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const b1 = document.getElementById('b1');
 
 b1.addEventListener("mouseover",() => {
-    b1.innerHTML='aukat ke bahar!';
+    b1.innerHTML='$400';
 });
 b1.addEventListener("mouseout", () => {
     b1.innerHTML='Buy Now';
@@ -17,49 +17,66 @@ b1.addEventListener("click", () => {
 const b2 = document.getElementById('b2');
 
 b2.addEventListener("mouseover",() => {
-    b2.innerHTML='aukat ke bahar!';
+    b2.innerHTML='$200';
 });
 b2.addEventListener("mouseout", () => {
     b2.innerHTML='Buy Now';
+});
+b2.addEventListener("click", () => {
+    window.open("transaction.html");
+    
 });
 
 const b3 = document.getElementById('b3');
 
 b3.addEventListener("mouseover",() => {
-    b3.innerHTML='aukat ke bahar!';
+    b3.innerHTML='$290';
 });
 b3.addEventListener("mouseout", () => {
     b3.innerHTML='Buy Now';
+});
+b3.addEventListener("click", () => {
+    window.open("transaction.html");
+    
 });
 
 const b4 = document.getElementById('b4');
 
 b4.addEventListener("mouseover",() => {
-    b4.innerHTML='aukat ke bahar!';
+    b4.innerHTML='$280';
 });
 b4.addEventListener("mouseout", () => {
     b4.innerHTML='Buy Now';
+});
+b4.addEventListener("click", () => {
+    window.open("transaction.html");
+    
 });
 
 const b5 = document.getElementById('b5');
 
 b5.addEventListener("mouseover",() => {
-    b5.innerHTML='aukat ke bahar!';
+    b5.innerHTML='$190';
 });
 b5.addEventListener("mouseout", () => {
     b5.innerHTML='Buy Now';
+});
+b5.addEventListener("click", () => {
+    window.open("transaction.html");
+    
 });
 
 const b6 = document.getElementById('b6');
 
 b6.addEventListener("mouseover",() => {
-    b6.innerHTML='aukat ke bahar!';
+    b6.innerHTML='$120';
 });
 b6.addEventListener("mouseout", () => {
     b6.innerHTML='Buy Now';
 });
-
-// clothing section
-
+b6.addEventListener("click", () => {
+    window.open("transaction.html");
+    
+});
 
 
